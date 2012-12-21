c540fc126a5aa6a4c27081f46cd1c9dc2ae8c63d
Rafał Studnicki <rafal.studnicki@erlang-solutions.com>
2012-12-21 10:22:15 +0100

    Divide semantic LBP and standard one

--- src/lbp.h
+++ src/lbp.h
@@ -6,9 +6,6 @@
 class LBP : public Descriptor {
   static auto convert_to_grayscale(Acc a) -> Acc;
   static auto threshold_masks(Acc a) -> Acc;
-  static auto perform_binning(Acc a) -> Acc;
-  static auto normalize_blocks(Acc a) -> Acc;
-  static auto flatten_features(Acc a) -> Acc;
 
   public:
   virtual vector<function<Acc(Acc)>> features(); 
